c66c4c1dd56c11e0c398e1aadc924d795bac31a7
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-06-14 10:23:30 -0700

    Add selection for new cars

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -27,7 +27,7 @@ public class CharacterSelect : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     float minX = 0;
     float maxX = 0;
-    int maxItems = 28;
+    int maxItems = 36;
     int currentVehicle = 0;
     float vehicleInstanceXInterval = 250f;
     private float lastDragPos;
